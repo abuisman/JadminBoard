33f77a7da8fc58c51c78d94185cf8d07475a7d16
Achilleas Buisman <accounts@abuisman.nl>
2012-11-26 12:01:47 +0100

    Added breadcrumbs

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,10 @@ class Admin::BaseController < ApplicationController
   before_filter :authenticate_admin!
   before_filter :set_sidebar
   
+  # You may want to move this to the ApplicationController if you use breadcrumbs-on-rails in the rest of the site
+  # In that case create some sort of check on the class of the controller.
+  add_breadcrumb "Dashboard", :admin_dashboard_path
+  
   layout 'admin'
 
   def set_sidebar
